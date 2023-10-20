8aca6c729ebdc1a0e8255a94e25046a8b11365c2
Jenny <jenniffer.chavez@gmail.com>
2023-10-20 13:45:28 -0500

    .

--- app/app.js
+++ app/app.js
@@ -1,22 +1,24 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
 const hostname = '134.209.168.175';
 const port = 7900;  // Reemplaza con el puerto que has habilitado
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
+  if (req.url === '/') {
+    // Lee el archivo HTML
+    const htmlPath = path.join(__dirname, 'index.html');
+    const htmlContent = fs.readFileSync(htmlPath, 'utf8');
 
-  // Lee el archivo HTML y envíalo como respuesta
-  fs.readFile('index.html', (err, data) => {
-    if (err) {
-      res.statusCode = 500;
-      res.end('Error interno del servidor');
-    } else {
-      res.end(data);
-    }
-  });
+    // Escribe la respuesta en el navegador
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(htmlContent);
+  } else {
+    // Manejar otros casos (por ejemplo, 404 Not Found)
+    res.writeHead(404);
+    res.end('Archivo no encontrado');
+  }
 });
 
 server.listen(port, hostname, () => {
